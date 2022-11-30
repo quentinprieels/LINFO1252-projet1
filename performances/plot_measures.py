c4e3c5a6414430680afde96d8d3e95ba9c999532
Quentin Prieels <quentin.prieels@gmail.com>
2022-11-30 21:47:48 +0100

    Solve errorbar problems in plot
    Previous program used min an max value, now it use the standard deviation

--- performances/plot_measures.py
+++ performances/plot_measures.py
@@ -6,7 +6,7 @@ This script plots the measures of the performances of a model.
 USAGE:   python3 plot_measures.py <performance_file> <output_file>
 EXAMPLE: python3 plot_measures.py performance.csv performance.pdf
 
-DESCRIPTION:    This script will analyze the performance file and plot the mean, max and min of the measures.
+DESCRIPTION:    This script will analyze the performance file and plot the mean an the standard deviation of the measures.
                 To generate the performance file, use the script performances/timeseries_measures.sh
                 The output file will be a file containing the plot (the extentions type must be given).
 """
@@ -21,17 +21,15 @@ name = sys.argv[2]
 
 df = pd.read_csv(filename, sep=',')
 
-# For each thread number, compute the mean, the max and the min values
+# For each thread number, compute the mean, the standard deviation
 df['means'] = df.drop('nbr_threads', axis=1).mean(axis=1)
-df['max'] = df.drop('nbr_threads', axis=1).max(axis=1)
-df['min'] = df.drop('nbr_threads', axis=1).min(axis=1)
+df['stds'] = df.drop('nbr_threads', axis=1).std(axis=1)
 
 # Plot the results
 plt.figure()
-plt.plot(df['nbr_threads'], df['means'], label='mean')
-plt.errorbar(df['nbr_threads'], df['means'], yerr=[df['means']-df['min'], df['max']-df['means']], fmt='o')
+plt.errorbar(df['nbr_threads'], df['means']*1000, yerr=df['stds']*1000, ecolor='red', linestyle='None', marker='.', markeredgewidth=2.5, capsize=5, capthick=1,)
 plt.xlabel('Number of threads')
-plt.ylabel('Time (s)')
+plt.ylabel('Time [ms]')
 plt.grid(True)
 plt.savefig(name)
 
