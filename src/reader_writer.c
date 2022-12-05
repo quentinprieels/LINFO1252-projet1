a540df8329c2f78e34284bc4fed63764b5a82745
nzeep7 <nathan.zeep@student.uclouvain.be>
2022-12-05 10:39:31 +0100

    adding the missing exit condition from the while's

--- src/reader_writer.c
+++ src/reader_writer.c
@@ -24,10 +24,10 @@ int writercount = 0; // nombre de writers actifs
 int readercount = 0; // nombre de readers actifs
 
 // Writer function
-void *writes(void *data) {
+void *writes(void *data){
     int count = *(int *) data;
 
-    while(count != 0) {
+    while(count != 0){
 
         // simule preparing the data
         for (int i=0; i<10000; i++);
@@ -35,7 +35,7 @@ void *writes(void *data) {
         // section critique donc on lock
         pthread_mutex_lock(&m_wcount);
         writercount++;
-        if (writercount == 1) {sem_wait(&rsem);} // arrivée du premier writer
+        if (writercount == 1){sem_wait(&rsem);} // arrivée du premier writer
         pthread_mutex_unlock(&m_wcount);
         
         sem_wait(&wsem);
@@ -45,18 +45,20 @@ void *writes(void *data) {
         // section critique donc on lock
         pthread_mutex_lock(&m_wcount);
         writercount--;
-        if (writercount == 0) {sem_post(&rsem);} // départ du dernier writer
+        if (writercount == 0){sem_post(&rsem);} // départ du dernier writer
         pthread_mutex_unlock(&m_wcount);
+
+        count--;
     }
 
     return 0;
 }
 
 // Reader function
-void *reads(void *data) {
+void *reads(void *data){
     int count = *(int *) data;
 
-    while(count != 0) {    
+    while(count != 0){    
 
         // lock pour empêcher des readers de s'accumuler sur rsem
         // pour que : quand un writer arrive il n'y ait qu'un lecteur d'occupé.
@@ -67,7 +69,7 @@ void *reads(void *data) {
         // section critique donc on lock
         pthread_mutex_lock(&m_rcount);
         readercount++;
-        if (readercount==1) {sem_wait(&wsem);} // arrivée du premier reader
+        if (readercount==1){sem_wait(&wsem);} // arrivée du premier reader
         pthread_mutex_unlock(&m_rcount);
 
         sem_post(&rsem);
@@ -78,11 +80,13 @@ void *reads(void *data) {
         // section critique donc on lock
         pthread_mutex_lock(&m_rcount);
         readercount--;
-        if(readercount==0) {sem_post(&wsem);} // départ du dernier reader
+        if(readercount==0){sem_post(&wsem);} // départ du dernier reader
         pthread_mutex_unlock(&m_rcount);
         
         // simule processing the data
         for (int i=0; i<10000; i++);
+
+        count--;
     }
 
     return 0;
@@ -90,7 +94,7 @@ void *reads(void *data) {
 
 int main(int argc, char const *argv[]){
     // Récupérer le nombre de writers et de readers (dans cet ordre)
-    if (argc != 3) {
+    if (argc != 3){
         printf("Vous n'avez pas donné correctement le nombre de writer suivi du nombre de readers.\n");
         exit(EXIT_FAILURE);
     }
@@ -98,25 +102,25 @@ int main(int argc, char const *argv[]){
     nbr_readers = atoi(argv[2]);
 
     //initialisation des mutex
-    if (pthread_mutex_init(&m_wcount, NULL) != 0) {
+    if (pthread_mutex_init(&m_wcount, NULL) != 0){
         printf("Erreur lors de l'initalisation du mutex wcount.");
         exit(EXIT_FAILURE);
     }
-    if (pthread_mutex_init(&m_rcount, NULL) != 0) {
+    if (pthread_mutex_init(&m_rcount, NULL) != 0){
         printf("Erreur lors de l'initalisation du mutex rcount.");
         exit(EXIT_FAILURE);
     }
-    if (pthread_mutex_init(&m_rsecurity, NULL) != 0) {
+    if (pthread_mutex_init(&m_rsecurity, NULL) != 0){
         printf("Erreur lors de l'initalisation du mutex rsecurity.");
         exit(EXIT_FAILURE);
     }
     
     //initialisation des sémaphores
-    if (sem_init(&wsem, 0, 1) == -1) {
+    if (sem_init(&wsem, 0, 1) == -1){
         printf("Erreur lors de l'initialisation du sémaphore des writers");
         exit(EXIT_FAILURE);
     }
-    if (sem_init(&rsem, 0, 1) == -1) {
+    if (sem_init(&rsem, 0, 1) == -1){
         printf("Erreur lors de l'initialisation du sémaphore des readers");
         exit(EXIT_FAILURE);
     }
@@ -126,12 +130,12 @@ int main(int argc, char const *argv[]){
     int first_towrite = toread_max - (towrite * nbr_writers ) + nbr_writers;
 
     pthread_t writers[nbr_writers];
-    if (pthread_create(&writers[0], NULL, writes, &first_towrite) != 0) {
+    if (pthread_create(&writers[0], NULL, writes, &first_towrite) != 0){
         fprintf(stderr, "Erreur lors de la creation d'un tread writer.\n");
         exit(EXIT_FAILURE);
     }
     for (int i = 1; i < nbr_writers; i++) {
-        if (pthread_create(&writers[i], NULL, writes, &towrite) != 0) {
+        if (pthread_create(&writers[i], NULL, writes, &towrite) != 0){
             fprintf(stderr, "Erreur lors de la creation d'un tread writer.\n");
             exit(EXIT_FAILURE);
         }
@@ -142,53 +146,53 @@ int main(int argc, char const *argv[]){
     int first_toread = toread_max - toread * (nbr_readers) + nbr_readers;
     
     pthread_t readers[nbr_readers];
-    if (pthread_create(&readers[0], NULL, reads, &first_toread) != 0) {
+    if (pthread_create(&readers[0], NULL, reads, &first_toread) != 0){
         fprintf(stderr, "Erreur lors de la creation d'un tread reader.\n");
         exit(EXIT_FAILURE);
     }
     for (int i = 1; i < nbr_readers; i++) {
-        if (pthread_create(&readers[i], NULL, reads, &toread) != 0) {
+        if (pthread_create(&readers[i], NULL, reads, &toread) != 0){
             fprintf(stderr, "Erreur lors de la creation d'un tread reader.\n");
             exit(EXIT_FAILURE);
         }
     }
 
     //join sur les threads writers
-    for (int i = 0; i < nbr_writers; i++) {
-        if (pthread_join(writers[i], NULL) != 0) {
+    for (int i = 0; i < nbr_writers; i++){
+        if (pthread_join(writers[i], NULL) != 0){
 			fprintf(stderr, "Erreur lors de la terminaison d'un writer.\n");
 			exit(EXIT_FAILURE);
 		}
     }
 
     //join sur les threads readers
-    for (int i = 0; i < nbr_readers; i++) {
-        if (pthread_join(readers[i], NULL) != 0) {
+    for (int i = 0; i < nbr_readers; i++){
+        if (pthread_join(readers[i], NULL) != 0){
 			fprintf(stderr, "Erreur lors de la terminaison d'un writer.\n");
 			exit(EXIT_FAILURE);
 		}
     }
 
     //destruction des mutex
-    if (pthread_mutex_destroy(&m_wcount) != 0) {
+    if (pthread_mutex_destroy(&m_wcount) != 0){
         printf("Erreur lors de la suppression du mutex wcount.");
         exit(EXIT_FAILURE);
     }
-    if (pthread_mutex_destroy(&m_rcount) != 0) {
+    if (pthread_mutex_destroy(&m_rcount) != 0){
         printf("Erreur lors de la suppression du mutex rcount.");
         exit(EXIT_FAILURE);
     }
-    if (pthread_mutex_destroy(&m_rsecurity) != 0) {
+    if (pthread_mutex_destroy(&m_rsecurity) != 0){
         printf("Erreur lors de la suppression du mutex rsecurity.");
         exit(EXIT_FAILURE);
     }
     
     //destruction des sémaphores
-    if (sem_destroy(&wsem) == -1) {
+    if (sem_destroy(&wsem) == -1){
         printf("Erreur lors de la suppression du sémaphore des writers");
         exit(EXIT_FAILURE);
     }
-    if (sem_destroy(&rsem) == -1) {
+    if (sem_destroy(&rsem) == -1){
         printf("Erreur lors de la suppression du sémaphore des readers");
         exit(EXIT_FAILURE);
     }
