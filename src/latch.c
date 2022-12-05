445df329399bc38b7cf42cd58669284e0180ce9d
Quentin Prieels <quentin.prieels@gmail.com>
2022-12-06 00:37:42 +0100

    First idea of test-and-test-and-set algorithm

--- src/latch.c
+++ src/latch.c
@@ -1,4 +1,4 @@
-/* ===== VERROU PAR ATTENTE ACTIVE ===== */
+/* ===== VERROU PAR ATTENTE ACTIVE TEST-AND-SET ===== */
 void lock(int *verrou) {
     int val = 1;
     
@@ -13,6 +13,33 @@ void lock(int *verrou) {
 }
 
 void unlock(int *verrou) {
+    int val = 0;
+
+    asm volatile(
+        "xchgl %0, %1"      /* Atomic exchange of values, set verrou to 0*/
+        : "+r" (val)
+        : "m" (*verrou)
+        : "memory"
+    );
+}
+
+/* ====== VERROU AVEC L'ALGORITHME TEST-AND-TEST-AND-SET =====*/
+void lock_test(int *verrou) {
+    int val = 1;
+
+    while(*verrou) {
+        if (val) {
+            asm volatile(
+                "xchgl %0, %1"      /* Atomic exchange of values */
+                : "+r" (val)        /* Operand referred to by %0, stored into any register (and writer/reading access) */
+                : "m" (*verrou)     /* Operand referred to by %1, stored into any register */
+                : "memory"          /* xchgl might have change the variabe at the memory location */
+            );
+        }
+    }
+}
+
+void unlock_test(int *verrou) {
     int val = 0;
 
     asm volatile(
