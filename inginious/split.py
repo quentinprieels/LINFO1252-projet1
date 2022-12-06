638f64f0ac1645ff3a63b9750dc3733d9f68aa1e
Quentin Prieels <quentin.prieels@gmail.com>
2022-12-06 19:52:09 +0100

    Script to convert run file of inginious into usable csv files

---
+++ inginious/split.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script split a csv file into several files.
+
+USAGE:   python3 split.py <input_file> 
+EXAMPLE: python3 split.py run1.csv
+
+DESCRIPTION:    This script will analyze the input file and split it into several files.
+                The output files will be named by the header of the time measure given in the input file.
+"""
+
+import sys
+
+# Read the datas
+filename = sys.argv[1]
+folder = "performances/"
+
+with open(filename, 'r') as input_file:
+    time_file = input_file.read()
+    time_file = time_file.split("\n\n")
+    
+    for i in range(len(time_file)):
+        header = time_file[i].split("\n")[0]
+        
+        # remove only the first line
+        measure = time_file[i][len(header)+1:]
+
+        with open(folder + header, 'w+') as output_file:
+            output_file.write(measure)
